ef7c40b91ef4f59fb3b1f0c8d9f72599d8e51b0f
alishahwee <alisha.hwee@outlook.com>
2020-05-26 21:15:54 -0500

    Fix UI and add responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Function to create n by n grid
 function createGrid(n) {
-  const container = document.querySelector("#grid-container");
+  const container = document.querySelector(".grid");
   for (let i = 0; i < n * n; i++) {
     let square = document.createElement("div");
-    container.appendChild(square).className = "grid-square";
+    container.appendChild(square).className = "grid__square";
   }
 }
 
