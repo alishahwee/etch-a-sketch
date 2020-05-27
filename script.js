c7726f5a9f5ee6f6492879050c4e6a94b5730f14
alishahwee <alisha.hwee@outlook.com>
2020-05-27 02:48:23 -0500

    Update style, add grid-change functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Select grid-container 
+// Select HTML elements
+const gridForm = document.querySelector(".grid-form");
 const container = document.querySelector(".grid");
+const gridSquares = document.querySelectorAll(".grid__square"); // Returns a NodeList
+const defaultBtn = document.querySelector(".sketch-menu__list__mode__button--default");
+const randomBtn = document.querySelector(".sketch-menu__list__mode__button--random");
+const transBtn = document.querySelector(".sketch-menu__list__mode__button--transparent");
+const clearBtn = document.querySelector(".sketch-menu__list__mode__button--clear-grid");
 
 // Function to create an n-by-n grid
 function createGrid(n) {
@@ -18,3 +24,14 @@ function createGrid(n) {
 
 // Render the default grid onto the document
 createGrid(16);
+
+// Change the grid upon form submission
+const changeGrid = (e) => {
+  let gridSize = e.target.elements["grid-size"].value;
+  container.innerHTML = '';
+  createGrid(gridSize);
+  e.preventDefault();
+}
+
+// Handle DOM events
+gridForm.addEventListener("submit", changeGrid);
