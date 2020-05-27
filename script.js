1448e4ba90074f9ea6b4aa41ced7703fad823972
alishahwee <alisha.hwee@outlook.com>
2020-05-26 22:24:28 -0500

    Fix responsiveness in respect to chosen grid size

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// Function to create n by n grid
+// Select grid-container 
+const container = document.querySelector(".grid");
+
+// Function to create an n-by-n grid
 function createGrid(n) {
-  const container = document.querySelector(".grid");
-  for (let i = 0; i < n * n; i++) {
+  // Ensure reasonable range input
+  if (n > 101) {
+    n = 100;
+  } else if (n < 1) {
+    n = 1;
+  }
+  for (let i = 0; i < (n * n); i++) {
+    container.style.setProperty('--grid-n', n);
     let square = document.createElement("div");
     container.appendChild(square).className = "grid__square";
   }
 }
 
+// Render the grid onto the document
 createGrid(16);
